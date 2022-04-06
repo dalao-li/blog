5ab82db273f6f84775dc5f0c7f026c0e52fb286e
dalao <dalao_li@163.com>
2022-04-06 22:57:31 +0800

    修改文件

---
+++ Linux/文件/write.c
@@ -0,0 +1,21 @@
+/*
+ * @Description: 
+ * @Version: 1.0
+ * @Author: dalao
+ * @Email: dalao_li@163.com
+ * @Date: 2022-04-06 22:47:55
+ * @LastEditors: dalao
+ * @LastEditTime: 2022-04-06 22:55:36
+ */
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+int main(void) {
+    int length = write(1 , "Here is some data\n" , 17);
+    if(length != 18) {
+        write(2 , "A write error has occurred on file descriptor 1\n" , 46);
+    }
+    return 0;
+}
