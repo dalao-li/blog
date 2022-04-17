2019b24b75f57e17798536ffcd8bd53bdcb615c2
dalao <dalao_li@163.com>
2022-04-17 09:45:28 +0800

    update

--- Linux/文件/write.c
+++ Linux/文件/write.c
@@ -5,7 +5,7 @@
  * @Email: dalao_li@163.com
  * @Date: 2022-04-06 22:47:55
  * @LastEditors: dalao
- * @LastEditTime: 2022-04-07 22:59:50
+ * @LastEditTime: 2022-04-17 09:44:47
  */
 
 #include<stdio.h>
@@ -13,7 +13,7 @@
 #include<unistd.h>
 
 int main(void) {
-    // fd1 输出
+    // 向fd1(屏幕)写入 
     int length = write(1 , "Here is some data\n" , 17);
     if(length != 18) {
         write(2 , "A write error has occurred on file descriptor 1\n" , 46);
